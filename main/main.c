80a5754fae9ee8712c7f4c7163cf53bbf0793616
Reka Kulcsar <reka.kulcsar.07@gmail.com>
2023-08-09 12:24:31 -0400

    Set up button interrupt handler to transition from ALARM to DISARMED state

--- main/main.c
+++ main/main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include "esp_log.h"
+#include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/queue.h"
-#include "freertos/timers.h"
-#include "freertos/semphr.h"
 
 #include "alarm.h"
 
@@ -13,6 +11,8 @@ static const char* TAG = "Main";
 static const int TASK_STACK_DEPTH = 16384;
 static const int SM_TASK_PRIORITY = 1;
 static const int TICK_PERIOD_MS = 250 / portTICK_PERIOD_MS;
+
+static const int PIN_BUTTON = 5;
 
 typedef enum
 {
@@ -29,7 +29,13 @@ typedef struct
 
 StateMachine sm;
 
-void sm_task_handler(void* arg)
+static void IRAM_ATTR button_interrupt_handler(void* args)
+{
+    if (sm.state == ALARM)
+        sm.state = DISARMED;
+}
+
+void sm_task_handler(void* args)
 {
     while (true)
     {
@@ -52,12 +58,19 @@ void sm_task_handler(void* arg)
     }
 }
 
-
 void init()
 {
     set_up_alarm();
 
-    sm.state = SETUP;
+    gpio_reset_pin(PIN_BUTTON);
+    gpio_set_direction(PIN_BUTTON, GPIO_MODE_INPUT);
+    gpio_pullup_dis(PIN_BUTTON);
+    gpio_pulldown_en(PIN_BUTTON);
+    gpio_set_intr_type(PIN_BUTTON, GPIO_INTR_LOW_LEVEL);
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(PIN_BUTTON, button_interrupt_handler, NULL);
+
+    sm.state = ALARM;
 
     xTaskCreate(&sm_task_handler, "StateMachine", TASK_STACK_DEPTH, NULL, 
         SM_TASK_PRIORITY, NULL);
