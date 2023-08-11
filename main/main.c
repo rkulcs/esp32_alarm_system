82f3a6bba94321efbf491836c8677f3354ba965c
Reka Kulcsar <reka.kulcsar.07@gmail.com>
2023-08-11 16:54:34 -0400

    Simplify state machine implementation

--- main/main.c
+++ main/main.c
@@ -3,11 +3,10 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#include "circular_buffer.h"
 #include "alarm.h"
 #include "sensor.h"
 
-static const int TASK_STACK_DEPTH = 16384;
-static const int SM_TASK_PRIORITY = 1;
 static const int TICK_PERIOD_MS = 250 / portTICK_PERIOD_MS;
 
 static const int PIN_BUTTON = 5;
@@ -20,26 +19,42 @@ typedef enum
     DISARMED
 } State;
 
-typedef struct
-{
-    State state;
-} StateMachine;
-
-StateMachine sm;
+State state;
 
 static void IRAM_ATTR button_interrupt_handler(void* args)
 {
-    if (sm.state == ALARMED)
-        sm.state = DISARMED;
+    if (state == ALARMED)
+        state = DISARMED;
 }
 
-void sm_task_handler(void* args)
+void init()
 {
+    set_up_alarm();
+    set_up_sensor();
+    init_circular_buffer(&distance_buffer);
+
+    gpio_reset_pin(PIN_BUTTON);
+    gpio_set_direction(PIN_BUTTON, GPIO_MODE_INPUT);
+    gpio_pullup_dis(PIN_BUTTON);
+    gpio_pulldown_en(PIN_BUTTON);
+    gpio_set_intr_type(PIN_BUTTON, GPIO_INTR_LOW_LEVEL);
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(PIN_BUTTON, button_interrupt_handler, NULL);
+
+    state = SETUP;
+}
+
+void app_main(void)
+{
+    init();
+
     while (true)
     {
-        switch (sm.state)
+        switch (state)
         {
         case SETUP:
+            sensor_set_safe_distances();
+            state = SENSING;
             break;
         case SENSING:
             break;
@@ -53,36 +68,5 @@ void sm_task_handler(void* args)
         }
 
         vTaskDelay(TICK_PERIOD_MS);
-    }
-}
-
-void init()
-{
-    set_up_alarm();
-    set_up_sensor();
-
-    gpio_reset_pin(PIN_BUTTON);
-    gpio_set_direction(PIN_BUTTON, GPIO_MODE_INPUT);
-    gpio_pullup_dis(PIN_BUTTON);
-    gpio_pulldown_en(PIN_BUTTON);
-    gpio_set_intr_type(PIN_BUTTON, GPIO_INTR_LOW_LEVEL);
-    gpio_install_isr_service(0);
-    gpio_isr_handler_add(PIN_BUTTON, button_interrupt_handler, NULL);
-
-    sm.state = SENSING;
-
-    xTaskCreate(&sm_task_handler, "StateMachine", TASK_STACK_DEPTH, NULL, 
-        SM_TASK_PRIORITY, NULL);
-}
-
-void app_main(void)
-{
-    init();
-
-    while (1) 
-    {
-        double distance_cm = sensor_get_distance_in_cm();
-        printf("%f\n", distance_cm);
-        vTaskDelay(100);
     }
 }
