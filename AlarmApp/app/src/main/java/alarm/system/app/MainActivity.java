fee8dd6c315e0704435c2431ed5b1c9c54333698
Reka Kulcsar <reka.kulcsar.07@gmail.com>
2023-09-04 15:55:30 -0400

    Fix notifications on Android 8.0+ devices

--- AlarmApp/app/src/main/java/alarm/system/app/MainActivity.java
+++ AlarmApp/app/src/main/java/alarm/system/app/MainActivity.java
@@ -1,12 +1,17 @@
 package alarm.system.app;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
@@ -21,13 +26,41 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String ALARM_TOPIC = "alarm";
 
+    private ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    Log.d(TAG, "Notification permission granted");
+                } else {
+                    Log.d(TAG, "Notification permission not granted");
+                }
+            }
+    );
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createNotificationChannel();
         subscribeToAlarmTopic();
+    }
+
+    /**
+     * Creates a notification channel on devices using Android 8.0+.
+     */
+    private void createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    getString(R.string.notification_channel_id),
+                    getString(R.string.notification_channel_name),
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
     }
 
     private void subscribeToAlarmTopic() {
