63926676877c18dfd85e4b023ef134e8d227d1cf
Reka Kulcsar <reka.kulcsar.07@gmail.com>
2023-09-30 22:53:59 -0400

    Add button for clearing alarm history

--- AlarmApp/app/src/main/java/alarm/system/app/history/NotificationsHistoryFragment.java
+++ AlarmApp/app/src/main/java/alarm/system/app/history/NotificationsHistoryFragment.java
@@ -3,12 +3,14 @@ package alarm.system.app.history;
 import static android.content.Context.MODE_PRIVATE;
 import static android.media.CamcorderProfile.getAll;
 
+import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
@@ -21,8 +23,10 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Set;
 
 import alarm.system.app.R;
@@ -45,6 +49,32 @@ public class NotificationsHistoryFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         notifications = new ArrayList<>();
+        loadNotificationsHistory();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.recycler_view_fragment, viewGroup, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        notificationsAdapter = new NotificationsAdapter(notifications);
+        recyclerView.setAdapter(notificationsAdapter);
+
+        Button clearButton = view.findViewById(R.id.clearButton);
+        clearButton.setOnClickListener(v -> {
+            clearNotificationsHistory();
+        });
+
+        return view;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void loadNotificationsHistory() {
 
         SharedPreferences sharedPreferences = getContext().getSharedPreferences(
                 getString(R.string.notifications_history_file), MODE_PRIVATE
@@ -59,22 +89,22 @@ public class NotificationsHistoryFragment extends Fragment {
                     Long.parseLong(time)).atZone(ZoneId.systemDefault()).toLocalDateTime()
             );
 
-            notifications.add(formattedTime);
+            String message = sharedPreferences.getString(time, "No message");
+            notifications.add(String.format("%s\n%s", formattedTime, message));
         }
     }
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup,
-                             Bundle savedInstanceState) {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void clearNotificationsHistory() {
 
-        View view = inflater.inflate(R.layout.recycler_view_fragment, viewGroup, false);
-        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences(
+                getString(R.string.notifications_history_file), MODE_PRIVATE
+        );
 
-        layoutManager = new LinearLayoutManager(getActivity());
-        recyclerView.setLayoutManager(layoutManager);
+        int numNotifications = sharedPreferences.getAll().size();
 
-        notificationsAdapter = new NotificationsAdapter(notifications);
-        recyclerView.setAdapter(notificationsAdapter);
-
-        return view;
+        sharedPreferences.edit().clear().apply();
+        notifications.clear();
+        recyclerView.getAdapter().notifyItemRangeRemoved(0, numNotifications);
     }
 }
